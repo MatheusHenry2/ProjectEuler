120f97c5f12807eb1751e8cc7758b0e8d008f7ad
MatheusHenry2 <71864649+MatheusHenry2@users.noreply.github.com>
2021-07-26 07:40:33 -0300

    Add files via upload

---
+++ Multiples of 3 or 5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+
+    for (int i = 1; i < 1000; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+            sum += i;
+    }
+    cout << sum << endl;
+}
